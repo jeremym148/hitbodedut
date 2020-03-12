725d49ed291865709a0db821b016c194b6698c10
Jeremy Martiano <jeremym@balink.net>
2020-03-13 00:06:48 +0200

    sss

--- client/src/components/utils.vue
+++ client/src/components/utils.vue
@@ -8,7 +8,7 @@ export default {
     },
     getHebrewCal(){
         let d = new Date();
-        let currentMonth = d.getMonth();
+        let currentMonth = d.getMonth() +1;
 
         axios
         .get("https://www.hebcal.com/hebcal/?v=1&cfg=json&year=now&month=" + currentMonth + "&geo=geoname&geonameid=281184&m=50&d=on")
